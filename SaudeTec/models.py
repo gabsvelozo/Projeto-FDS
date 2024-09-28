c33238bd9257f7f912e4faf9201256fc5e317e2a
luiscoast <lacc2@cesar.school>
2024-09-28 12:02:32 -0300

    Models e HTML da pagina checklist

--- SaudeTec/models.py
+++ SaudeTec/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class ficha_pre_consulta(models.Model):
+    queixa_principal = models.TextField(max_length=1000)
+    causa_da_queixa = models.TextField(max_length=1000)
+    medida_de_tempo = models.CharField(max_length=50)
+    duracao_sintomas = models.IntegerField(max_length=50)
+    intensidade_da_dor = models.IntegerField()
+    constancia_da_dor = models.TextField(max_length=50)
+    agravantes_da_molestia = models.TextField(max_length = 1000)
