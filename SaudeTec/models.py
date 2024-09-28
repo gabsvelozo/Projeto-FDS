3bde2dc8436e7f228599aedf3d2fde24f51ee7c7
luiscoast <lacc2@cesar.school>
2024-09-28 12:47:35 -0300

    Alteração do models e do views

--- SaudeTec/models.py
+++ SaudeTec/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
-from django.db import models
-
-class ficha_pre_consulta(models.Model):
-    queixa_principal = models.TextField(max_length=1000)
-    causa_da_queixa = models.TextField(max_length=1000)
-    medida_de_tempo = models.CharField(max_length=50)
-    duracao_sintomas = models.IntegerField(max_length=50)
-    intensidade_da_dor = models.IntegerField()
-    constancia_da_dor = models.TextField(max_length=50)
-    agravantes_da_molestia = models.TextField(max_length = 1000)
+class Ficha_Pre_Consulta(models.Model):
+    motivo = models.TextField(max_length=1000)
+    periodo = models.CharField(max_length=1000)
+    dor = models.IntegerField()
+    quando_dor = models.TextField(max_length=50)
+    algo_mais = models.TextField(max_length = 1000)
--- SaudeTec/views.py
+++ SaudeTec/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Ficha_Pre_Consulta
 
-# Create your views here.
+class CheckListView(View):
+    def get(self, request):
+        return render(request, 'checklist.html')
+
+    def post(self, request):
+        motivo = request.POST.get("motivo")
+        periodo = request.POST.get("periodo")
+        dor = request.POST.get("dor")
+        quando_dor = request.POST.get("quando_dor")
+        algo_mais = request.POST.get("algo_mais")
+
+        ficha_pre_consulta = Ficha_Pre_Consulta(
+            motivo = motivo
+            periodo = periodo
+            dor = dor
+            quando_dor = quando_dor
+            algo_mais = algo_mais
+        )
+
+        ficha_pre_consulta.save()
+
+        return redirect('SaudeTec:home')
