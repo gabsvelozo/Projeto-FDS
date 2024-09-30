1b5d6cccc68056c948132a2c9d24d3db00bb6d83
Sofia1653 <124153233+Sofia1653@users.noreply.github.com>
2024-09-30 09:48:32 -0300

    Delete saude directory

--- saude/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Especialidade, Local, Consulta, SintomasUsuario
-
-admin.site.register(Especialidade)
-admin.site.register(Local)
-admin.site.register(Consulta)
-admin.site.register(SintomasUsuario)
--- saude/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'Saude'
-
-urlpatterns = [
-    path('historico/', views.HistoricoOnline.as_view(), name='historico'),
-    path('sucesso/', views.Sucesso.as_view(), name='sucesso'),
-]
