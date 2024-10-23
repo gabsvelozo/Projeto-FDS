27f127dfc3b7fffbf15f6546f95479c6c0b0ff0d
Sofia1653 <sofedu2005@gmail.com>
2024-10-23 20:56:21 -0300

    adição do cypress

--- cypress/e2e/saude.cy.js
+++ cypress/e2e/saude.cy.js
@@ -1,47 +1,103 @@
-describe('cadastro e login',()=> {
-    it('histórico com todas as informações', ()=> {
+describe('agendamento de consultas',()=> {
+    it('agendamento de consultas de forma certa', ()=> {
         cy.visit('/');
-        cy.get('#username').type('luis')
-        cy.get('#password').type('1234')
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
         cy.get('.bg-blue-500').click()
-        cy.get(':nth-child(1) > .block').click()
-        cy.get('#nome').type('luis')
-        cy.get('#idade').type('18')
-        cy.get('#medicacao').type('paracetamol')
-        cy.get('#doencas').type('diabetes')
-        cy.get('#cirugias').type('rinoplastia')
-        cy.get('#alergias').type('amendoim')
-        cy.get('.bg-blue-500').click()
+        cy.get(':nth-child(2) > .block').click()
+        cy.get('#especialidade').select('Ginecologia')
+        cy.get('#local').select('UPAE-R')
+        cy.get('#data').click()
+        cy.get('.px-4 > .w-full').click()
     })  
-    it('histórico com duas variaveis', ()=> {
+    it('entrado em agendamento sem enviar nada', ()=> {
         cy.visit('/');
-        cy.get('#username').type('luis')
-        cy.get('#password').type('1234')
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
         cy.get('.bg-blue-500').click()
-        cy.get(':nth-child(1) > .block').click()
-        cy.get('#nome').type('luis eduardo')
-        cy.get('#idade').type('20')
-        cy.get('#medicacao').type('paracetamol e vitamina D')
-        cy.get('#doencas').type('hipertensão e diabetes')
-        cy.get('#cirugias').type('lipoaspiração e hernia de disco')
-        cy.get('#alergias').type('acaro e leite')
-        cy.get('.bg-blue-500').click()
-    }) 
-    it('historico sem nome', ()=> {
+        cy.get(':nth-child(2) > .block').click()
+        cy.get('.ml-2').click()
+    })
+    it('entrado em consultas pelo agendamento', ()=> {
         cy.visit('/');
-        cy.get('#username').type('luis')
-        cy.get('#password').type('1234')
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
         cy.get('.bg-blue-500').click()
-        cy.get(':nth-child(1) > .block').click()
-        cy.get('#nome').should('have.value', '').then(() => {
-            throw new Error('O campo não deve estar vazio, teste falhou!');
-          });
-        cy.get('#idade').type('20')
-        cy.get('#medicacao').type('paracetamol e vitamina D')
-        cy.get('#doencas').type('hipertensão e diabetes')
-        cy.get('#cirugias').type('lipoaspiração e hernia de disco')
-        cy.get('#alergias').type('acaro e leite')
-        cy.get('.bg-blue-500').click()
-    }) 
+        cy.get(':nth-child(2) > .block').click()
+        cy.get('.ml-10 > [href="/consultas/"]').click()
+    })
+})
 
+describe('ver minhas consultas',()=> {
+    it('ver as consultas', ()=> {
+        cy.visit('/');
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
+        cy.get('.bg-blue-500').click()
+        cy.get(':nth-child(3) > .block').click()
+        cy.get('.border-b > :nth-child(1)')
+        cy.get('.border-b > :nth-child(2)')
+        cy.get('.border-b > :nth-child(3)')
+        cy.get('div.items-center > :nth-child(2) > .text-xl').click()
+    })  
+    it('saindo de consultas e entrando em agendamentos', ()=> {
+        cy.visit('/');
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
+        cy.get('.bg-blue-500').click()
+        cy.get(':nth-child(3) > .block').click()
+        cy.get('.border-b > :nth-child(1)')
+        cy.get('.border-b > :nth-child(2)')
+        cy.get('.border-b > :nth-child(3)')
+        cy.get('.ml-10 > [href="/agendamento/"]').click()
+    })
+    it('visualizando as consultas', ()=> {
+        cy.visit('/');
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
+        cy.get('.bg-blue-500').click()
+        cy.get(':nth-child(3) > .block').click()
+        cy.get('.border-b > :nth-child(1)')
+        cy.get('.border-b > :nth-child(2)')
+        cy.get('.border-b > :nth-child(3)')
+        cy.get('div.items-center > :nth-child(2) > .text-xl').click()
+    })
+})
+
+describe('localização',()=> {
+    it('escolhendo o local', ()=> {
+        cy.visit('/');
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
+        cy.get('.bg-blue-500').click()
+        cy.get(':nth-child(6) > .block').click()
+        cy.get('#bairro-select').select('Boa Viagem')
+        cy.get('div.px-4 > .w-full').click()
+        cy.get(':nth-child(2) > .text-xl')
+        cy.get('#informacoes > :nth-child(2) > :nth-child(3)')
+    })  
+    it('saindo pela barra embaixo', ()=> {
+        cy.visit('/');
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
+        cy.get('.bg-blue-500').click()
+        cy.get(':nth-child(6) > .block').click()
+        cy.get('#bairro-select').select('Casa Amarela')
+        cy.get('div.px-4 > .w-full').click()
+        cy.get(':nth-child(2) > .text-xl')
+        cy.get('#informacoes > :nth-child(2) > :nth-child(3)')
+        cy.get('.text-blue-500').click()
+    })
+    it('usando pela nav bar', ()=> {
+        cy.visit('/');
+        cy.get('#username').type('gabi')
+        cy.get('#password').type('gabi')
+        cy.get('.bg-blue-500').click()
+        cy.get(':nth-child(6) > .block').click()
+        cy.get('#bairro-select').select('Estância')
+        cy.get('div.px-4 > .w-full').click()
+        cy.get(':nth-child(2) > .text-xl')
+        cy.get('#informacoes > :nth-child(2) > :nth-child(3)')
+        cy.get('.ml-2').click()
+    })
 })
